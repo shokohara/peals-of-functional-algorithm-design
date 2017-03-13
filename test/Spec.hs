0bbf4a22526ec1bc5e6757cc334fcd76a853aba5
shokohara <shokohara@users.noreply.github.com>
2017-03-13 18:10:36 +0900

    Initial commit

---
+++ test/Spec.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Data.FileEmbed (embedFile)
+import Test.Hspec
+import Data.Maybe
+import Data.Char
+import qualified Data.ByteString.Char8 as B
+
+main :: IO ()
+main = hspec $ return ()
